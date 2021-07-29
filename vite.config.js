1306b8ec24330f971f629446950a1dc3bb9ae02a
Giovanni Bronca <42703789+gbronca@users.noreply.github.com>
2021-07-29 21:58:47 +0100

    moved the source files into a src folder and updated the vite.config.js file to include all html files in production

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,33 @@
-import { resolve } from 'path';
+import path from 'path';
+// import glob from 'glob';
 import handlebars from 'vite-plugin-handlebars';
 
-// const pageData = {
-// 	'/index.html': {
-// 		title: 'Main Page',
-// 	},
-// 	'/nested/subpage.html': {
-// 		title: 'Sub Page',
-// 	},
-// };
-
 export default {
+	root: path.resolve(__dirname, 'src'),
+	build: {
+		outDir: path.join(__dirname, 'dist'),
+		emptyOutDir: true,
+		rollupOptions: {
+			input: {
+				main: path.resolve(__dirname, '/index.html'),
+				about: path.resolve(__dirname, '/about.html'),
+				contact: path.resolve(__dirname, '/contact.html'),
+				friend: path.resolve(__dirname, '/friend.html'),
+				initiatives: path.resolve(__dirname, '/initiatives.html'),
+				media: path.resolve(__dirname, '/media.html'),
+				membership: path.resolve(__dirname, '/membership.html'),
+				team: path.resolve(__dirname, '/team.html'),
+				connections: path.resolve(__dirname, '/blogs/connections.html'),
+				'changing-the-workplace-environment': path.resolve(
+					__dirname,
+					'/blogs/changing-the-workplace-environment.html'
+				),
+			},
+		},
+	},
 	plugins: [
 		handlebars({
-			// context(pagePath) {
-			// 	return pageData[pagePath];
-			// },
-			partialDirectory: resolve(__dirname, 'partials'),
+			partialDirectory: path.resolve(__dirname, 'src', 'partials'),
 		}),
 	],
 };
