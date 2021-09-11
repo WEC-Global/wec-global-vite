009a527e10991290cc72c0c777deba755f2c4e41
Giovanni Bronca <42703789+gbronca@users.noreply.github.com>
2021-09-11 16:51:10 +0100

    Added new images, updated headers, updated CSS

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import path from 'path';
 import glob from 'glob';
 import handlebars from 'vite-plugin-handlebars';
-import copy from 'rollup-plugin-copy';
 
 // Add the root folder and the blogs folder to rollupOptions.input
 rootFolders = glob.sync(path.resolve(__dirname, 'src', '*.html'));
@@ -17,34 +16,11 @@ export default {
 		emptyOutDir: true,
 		rollupOptions: {
 			input: inputFolders,
-			// input: {
-			// 	main: path.resolve(__dirname, '/index.html'),
-			// 	about: path.resolve(__dirname, '/about.html'),
-			// 	contact: path.resolve(__dirname, '/contact.html'),
-			// 	friend: path.resolve(__dirname, '/friend.html'),
-			// 	initiatives: path.resolve(__dirname, '/initiatives.html'),
-			// 	media: path.resolve(__dirname, '/media.html'),
-			// 	membership: path.resolve(__dirname, '/membership.html'),
-			// 	team: path.resolve(__dirname, '/team.html'),
-			// 	connections: path.resolve(__dirname, '/blogs/connections.html'),
-			// 	'changing-the-workplace-environment': path.resolve(
-			// 		__dirname,
-			// 		'/blogs/changing-the-workplace-environment.html'
-			// 	),
-			// },
 		},
 	},
 	plugins: [
 		handlebars({
 			partialDirectory: path.resolve(__dirname, 'src', 'partials'),
-		}),
-		copy({
-			targets: [
-				{ src: './src/assets/docs', dest: './dist/assets/docs' },
-				{ src: './src/assets/img/*', dest: './dist/assets/img' },
-			],
-			flatten: false,
-			hook: 'writeBundle',
 		}),
 	],
 };
